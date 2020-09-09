b80c04a3fd4cffb4524c7cf77e14faea2d8c5453
jleeson <jleeson2000@gmail.com>
2020-09-08 17:40:16 -0700

    cleaned up exports

--- packages/jolt/src/index.js
+++ packages/jolt/src/index.js
@@ -1,25 +1,3 @@
-/* imports */
-import { TemplateEngine } from "./runtime/TemplateEngine";
-import { Reconciler } from "./runtime/Reconciler";
-
-/**
- * Creates a {@link Template} to be rendered.
- * @param {TemplateStringsArray} strings 
- * @param  {...*} values
- * @return {Template}
- */
-export function html(strings, ...values) {
-    return TemplateEngine.createTemplate(strings, values);
-}
-
-/**
- * Renders the component to the container element.
- * @param {CustomElementConstructor|Function} component 
- * @param {HTMLElement} container 
- */
-export function render(component, container) {
-    if(component.selector) Reconciler.reconcile(html`<${component.selector}></${component.selector}>`, container);
-    else console.warn("Jolt: Components must be registered before being rendered.");
-}
-
+/* exports */
 export { Component } from "./components/Component";
+export { html } from "./runtime/TemplateEngine";
