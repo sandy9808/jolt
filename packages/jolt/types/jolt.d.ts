e8fdead91fe8b8b9f2a8069a72c74cf13464e0a4
jleeson <jleeson2000@gmail.com>
2020-09-11 16:35:33 -0700

    added types

---
+++ packages/jolt/types/jolt.d.ts
@@ -0,0 +1,49 @@
+declare type StateCallback = (key?: string | number | symbol, value?: any) => void;
+
+declare class State {
+    private prototype: any;
+
+    set(state: object): void;
+    static createState(callback: StateCallback): State;
+    static createArrayProxy(array: Array<any>, callback: StateCallback): Array<any>;
+}
+
+declare interface Attributes {
+    [key: string]: string;
+}
+
+declare interface ComponentOptions {
+    name: string,
+    styles?: Array<string>,
+    useShadow?: boolean;
+}
+
+declare interface Template {
+    source: string;
+    events: Array<Function>
+}
+
+declare abstract class Component<T> extends HTMLElement {
+
+    private _observer: MutationObserver;
+
+    root: T;
+    attribs: Attributes;
+
+    private connectedCallback(): void;
+    private disconnectedCallback(): void;
+
+    abstract render(attribs?: Attributes): Template;
+
+    didLoad(): void;
+    didUpdate(): void;
+    shouldUpdate(): boolean;
+    willUnload(): void;
+
+    static create(options: ComponentOptions, component: CustomElementConstructor | Function): void;
+    static mount(component: CustomElementConstructor | Function, container: HTMLElement): void;
+}
+
+declare function html(strings: TemplateStringsArray, ...values: Array<any>): Template;
+
+export { Component, State, Attributes, Template, html };
