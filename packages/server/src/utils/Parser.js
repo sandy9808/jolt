97fe46b63445411cbb9e9420a1e5ce775a82252c
jleeson <jleeson2000@gmail.com>
2020-09-17 16:23:45 -0700

    fixed cli parser

--- packages/server/src/utils/Parser.js
+++ packages/server/src/utils/Parser.js
@@ -13,29 +13,27 @@ export class Parser {
         const tmp = {};
         tmp._ = [];
 
-        let arg;
-        let newArg;
+        let arg = null;
+        let newArg = [];
 
-        const length = args.length;
-        for (let i = 0; i < length; i++) {
+        for (let i = 0; i < args.length; i++) {
             arg = args[i];
             newArg = [];
 
-            if(arg.startsWith("-") && !arg.startsWith("--")) {
-                arg.replace(/-/, "--");
+            if (arg.startsWith("-") && !arg.startsWith("--")) {
+                arg = arg.replace(/-/g, "--");
             }
 
-            if(arg.startsWith("--")) {
+            if (arg.startsWith("--")) {
                 newArg.push(arg.slice(2));
                 const tmpArg = args[i + 1];
 
-                if(tmpArg != null && !tmpArg.startsWith("--")) {
+                if (tmpArg != null && !tmpArg.startsWith("--")) {
                     newArg.push(tmpArg);
                     args.slice(args.indexOf(tmpArg), 1);
                 } else {
                     newArg.push(true);
                 }
-
                 tmp[newArg[0]] = newArg[1];
             } else {
                 tmp._.push(arg);
