58a4a72912bb63f50493d89e940e366f9fae1f18
jleeson <jleeson2000@gmail.com>
2020-09-17 17:30:38 -0700

    mapped commands to functions

--- packages/cli/src/index.js
+++ packages/cli/src/index.js
@@ -3,19 +3,31 @@ import { Parser } from "./utils/Parser";
 import version from "./cmd/version";
 import help from "./cmd/help";
 import create from "./cmd/create";
+import update from "./cmd/update";
+import repair from "./cmd/repair";
+import build from "./cmd/build";
+import watch from "./cmd/watch";
+import lint from "./cmd/lint";
+import serve from "./cmd/serve";
 
 /* parse the cli arguments */
 const args = Parser.parseArguments(process.argv.slice(2));
 let cmd = args._[0] || "help";
 
 /* determine what action to run */
-if(args.version || args.v) cmd = "version";
+if (args.version || args.v) cmd = "version";
 else if (args.help || args.h) cmd = "help";
 
 const commands = {
     "version": version,
     "help": help,
-    "create": create
+    "create": create,
+    "update": update,
+    "repair": repair,
+    "build": build,
+    "watch": watch,
+    "lint": lint,
+    "serve": serve
 };
 
 if (commands[cmd]) commands[cmd](args);
